17ef9c260ee56e2af57b18d11da1576ae2a37287
Aman Shahi <amanshahidev@gmail.com>
2025-07-09 15:43:03 +0530

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -26,7 +26,6 @@ twitterBtn.addEventListener('click', () => {
 
 const links = document.querySelectorAll('.link');
 
-// STEP 1: Add click event to save active tab
 links.forEach(link => {
   link.addEventListener('click', function () {
     const href = this.getAttribute('href');
@@ -34,7 +33,6 @@ links.forEach(link => {
   });
 });
 
-// STEP 2: On page load, highlight the active tab
 const currentActive = localStorage.getItem('isActiveTab');
 
 links.forEach(link => {
