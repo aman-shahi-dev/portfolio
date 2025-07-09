8cbb6bcc688914f949817ddb18f98708f64d28f1
Aman Shahi <amanshahidev@gmail.com>
2025-07-09 18:48:13 +0530

    updated the skills tab

--- scripts/skills.js
+++ scripts/skills.js
@@ -1,7 +1,6 @@
 
 const links = document.querySelectorAll('.link');
 
-// STEP 1: Add click event to save active tab
 links.forEach(link => {
   link.addEventListener('click', function () {
     const href = this.getAttribute('href');
@@ -9,7 +8,6 @@ links.forEach(link => {
   });
 });
 
-// STEP 2: On page load, highlight the active tab
 const currentActive = localStorage.getItem('isActiveTab');
 
 links.forEach(link => {
